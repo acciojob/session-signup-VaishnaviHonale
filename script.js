a4c2f3b45a11758b7db5eef1a4393b59a32dcf80
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-09-25 14:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+ document.getElementById("signup-form").addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            const name = document.getElementById("name").value;
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            const confirmPassword = document.getElementById("confirm-password").value;
+
+            if (password === confirmPassword) {
+                const userData = {
+                    name: name,
+                    email: email,
+                    password: password
+                };
+
+                // Store data in session storage
+                sessionStorage.setItem("name", userData.name);
+                sessionStorage.setItem("email", userData.email);
+                sessionStorage.setItem("password", userData.password);
+
+                alert("Sign up successful!");
+            } else {
+                alert("Passwords do not match");
+            }
+ });
